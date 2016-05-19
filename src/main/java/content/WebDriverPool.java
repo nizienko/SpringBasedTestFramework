d643c93189b2e72c74ce8b2f714c80323d58d0e3
nizienko <def.cccp@gmail.com>
2016-05-19 18:22:34 +0300

    block

--- src/main/java/content/WebDriverPool.java
+++ src/main/java/content/WebDriverPool.java
@@ -1,9 +1,14 @@
 package content;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by def on 16.05.2016.
@@ -29,6 +34,23 @@ public class WebDriverPool {
 
 
     private WebDriver createWebDriver(){
-        return new FirefoxDriver();
-    }
+        WebDriver webDriver;
+        if (System.getProperty("browser") == null || System.getProperty("browser").equals("")) {
+            System.setProperty("browser", "firefox");
+        }
+
+        if ("firefox".equals(System.getProperty("browser"))) {
+            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+            capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            webDriver = new FirefoxDriver(capabilities);
+        } else {
+            if (System.getProperty("webdriver.chrome.driver") == null) {
+                System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+            }
+            DesiredCapabilities caps = DesiredCapabilities.chrome();
+            caps.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            webDriver = new ChromeDriver(caps);
+        }
+        webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        return webDriver;    }
 }
--- src/test/java/Suite2Test.java
+++ src/test/java/Suite2Test.java
@@ -1,11 +1,19 @@
+import content.steps.CommonSteps;
 import content.steps.GoogleSteps;
+import core.ExecutionListener;
+import core.FrameworkConfiguration;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by def on 17.05.2016.
  */
-public class Suite2Test extends Suite {
+
+public class Suite2Test extends Suite{
 
     @Autowired
     private GoogleSteps googleSteps;
